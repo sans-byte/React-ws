05c8779344bc31dcafceb47fa48f3fb028388ca5
sans-byte <sanskarj15@gmail.com>
2024-09-01 12:17:06 +0530

    made a basic chat app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [message, setMessage] = useState();
 
   useEffect(() => {
     const socket = new WebSocket("ws://localhost:8080");
@@ -12,14 +13,33 @@ function App() {
     };
     socket.onmessage = (message) => {
       console.log("Getting message", message.data);
+      setMessage(message.data);
+    };
+
+    return () => {
+      socket.close();
     };
   }, []);
+
+  const ref = useRef<HTMLInputElement>(null);
 
   if (!socket) {
     return <div> Loading... </div>;
   }
-  
-  return <></>;
+  return (
+    <>
+      <input type="text" ref={ref} />
+      <button
+        onClick={() => {
+          socket.send(ref.current?.value!);
+        }}
+      >
+        Send
+      </button>
+
+      <div>{message}</div>
+    </>
+  );
 }
 
 export default App;
